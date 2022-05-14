a2295b689c882a39966be832e206141e402e7cdb
Santiago Toloza <santiago.b.toloza@gmail.com>
2022-05-14 19:58:22 -0300

    tratando de agregar el img

--- src/components/GridScan.jsx
+++ src/components/GridScan.jsx
@@ -1,9 +1,10 @@
 import "../../css/GridScan.css"
+import accept from '../../images/accept.svg'
 export const GridScan = () => {
   return (
 
 <div className="scan" id="scanJump">
-        <img src="\images\accept.svg" alt="" className="hero2" />
+        <img src={accept} alt="" className="hero2" />
         <div className="texto-scanear grid  " >
             <h1>Escanee el codigo QR asdfas</h1>
             <p>Puede escanear cualquier codigo QR desde cualquier dispositivo movil
