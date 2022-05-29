47023e72768b8cd44dea139fb1da02661ba0f413
Santiago Toloza <santiago.b.toloza@gmail.com>
2022-05-29 03:25:13 -0300

    proyecto terminado qr

--- src/components/GridScan.jsx
+++ src/components/GridScan.jsx
@@ -4,18 +4,18 @@ export const GridScan = () => {
   return (
     <>
       <div className="scanqr grid">
-  
+        <div className="img2">
           <img src={accept} alt="" className="hero2" />
+        </div>
         <div className="texto-scanear ">
-            <h1>Escanee el codigo QR</h1>
-            <p>
-              Puede escanear cualquier codigo QR desde cualquier dispositivo
-              movil Solo enfoque el QR y se le brindara el texto
-            </p>
-            <a href="/scanApp" className="btnscan">
-              Escanear
-            </a>
-          
+          <h1>Escanee el codigo QR</h1>
+          <p>
+            Puede escanear cualquier codigo QR desde cualquier dispositivo movil
+            Solo enfoque el QR y se le brindara el texto
+          </p>
+          <a href="/scanApp" className="btnscan">
+            Escanear
+          </a>
         </div>
       </div>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,35 @@
-import "../../css/Navbar.css"
-import "../../css/MediaQ.css"
+import "../../css/Navbar.css";
+import "../../css/MediaQ.css";
+import { Link } from "react-router-dom";
 import { useState } from "react";
-  
 
+export const Navbar = () => {
+  const [menu, setMenu] = useState(false);
 
-
-export const Navbar = () => {   
-    const [menu, setMenu] = useState(false)
-
-    const toggle = ()=>{
-    setMenu(!menu)
-}   
+  const toggle = () => {
+    setMenu(!menu);
+  };
   return (
     <>
-    <nav className="navbar">
-      <ul className= {"menu " + (menu ? 'show ' : '')  } id="navUL" >
-        <li>
-          <a href="#home">Home</a>
-        </li>
-        <li>
-          <a href="#home">Create QR</a>
-        </li>
-        <li>
-          <a href="#scanJump">Scan QR</a>
-        </li>
-        <li>
-          <a href="#footscroll">Contact</a>
-        </li>
-      </ul>
-      <div className={"menu-bar" + (menu ? ' mostrar' : '' )} >
-        <i className="fa-solid fa-bars" onClick={toggle} ></i>
-      </div>
-    </nav>
+      <nav className="navbar">
+        <ul className={"menu " + (menu ? "show " : "")} id="navUL">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/APPQR">APP QR</Link>
+          </li>
+          <li>
+            <Link to="/SCANAPP">SCAN APP</Link>
+          </li>
+          <li>
+            <a href="#footscroll">Contact</a>
+          </li>
+        </ul>
+        <div className={"menu-bar" + (menu ? " mostrar" : "")}>
+          <i className="fa-solid fa-bars" onClick={toggle}></i>
+        </div>
+      </nav>
     </>
   );
 };
